c142d4ce159104268b16463e597ed5444e612e6b
anavheoba <wisdomabraham92@gmail.com>
2025-05-08 14:38:33 -0400

    pos

--- src/config/firebase/firebase.ts
+++ src/config/firebase/firebase.ts
@@ -1,16 +1,23 @@
 import * as admin from 'firebase-admin';
-import * as serviceAccount from './serviceAccountKey.json';
+import { config } from '../environment';
 
-// Initialize Firebase Admin
+const serviceAccount = {
+  type: config.firebase.type,
+  project_id: config.firebase.projectId,
+  private_key_id: config.firebase.privateKeyId,
+  private_key: config.firebase.privateKey,
+  client_email: config.firebase.clientEmail,
+  client_id: config.firebase.clientId,
+  auth_uri: config.firebase.authUri,
+  token_uri: config.firebase.tokenUri,
+  auth_provider_x509_cert_url: config.firebase.authProviderX509CertUrl,
+  client_x509_cert_url: config.firebase.clientX509CertUrl,
+  universe_domain: config.firebase.universeDomain
+};
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
 });
 
-// Initialize Firestore
-const db = admin.firestore();
-
-// Initialize Firebase Cloud Messaging
-const messaging = admin.messaging();
-
-export { admin, db, messaging }; 
+export const db = admin.firestore();
+export const messaging = admin.messaging();
